bdf732bf5d5cfbc40de2a57cd7d991bee18007c8
Jabievaitakin <144376647+Jabievaitakin@users.noreply.github.com>
2024-04-18 02:13:23 +0400

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+// let result=1
+// let num =5
+// for (let i = 1; i <=num; i++) {
+//     result=i*result
+// }
+// console.log(result)
+
+
+// let result=""
+// for (let i = 1; i<=5; i++) {
+  
+//    stars="*"
+//    result=result+stars
+// }
+// console.log(result)
+
+
+
+// for (let i = 1; i<=9; i++) {
+//     for (let j = 1; j<=9; j++) {
+        
+//         console.log(`${i}*${j} =${j*i}`);
+//     }
+// }
+
+
+
+
+
